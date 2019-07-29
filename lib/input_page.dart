3f9a8732d69d2d0bfba498dd486afec4cc51ae45
Rodrigo Castro <rodrigo.castro@easynvest.com.br>
2019-07-29 13:24:52 -0300

    implement reusable widget

---
+++ lib/input_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+              ]),
+            ),
+            Expanded(
+              child: ReusableWidget(color: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableWidget(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableWidget(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class ReusableWidget extends StatelessWidget {
+  ReusableWidget({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
